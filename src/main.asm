ef4a857214f036a3e696f88ca7542da164ab804f
Inas Malkic <inas.malkic@bicomsystems.com>
2023-04-02 16:28:44 +0200

    Sum of numbers

--- src/main.asm
+++ src/main.asm
@@ -38,6 +38,17 @@ prompt:
     call puts
     ret
 
+
+newline:
+    mov ah, 0x0e
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+    ret
+
+
+
 main:
     mov ax, 0
     mov ds, ax
@@ -46,39 +57,56 @@ main:
     mov sp, 0x7C00
     call prompt
     call getchar
-    mov [name], al
-    cmp al, 0x31
-    je .is_one
-    jmp .not_one
+    sub al, '0'
+    mov [num1], al
+    call printnum
+    call newline
+
+    call getchar
+    sub al, '0'
+    mov [num2], al
+    call printnum
+    call newline
+
+    mov al, [num1]
+    add al, [num2]
+    mov ah, 0
+    adc ah, 0
+    add al, '0'
+    add ah, '0'
+    mov [result], ax
+    call putchar
+    call newline
+
     hlt
 
 
-.is_one:
-    mov si, msg_one
-    call puts
-    jmp read_input
+printnum:
+    mov ah, 0x0e  
+    mov bh, 0     
+    mov bl, 7
+    add al, '0'
+    int 0x10     
+    ret
 
 
-.not_one:
-    cmp al, 0x0D
-    je .halt
-    mov si, msg_not_one
-    call puts
-    jmp read_input
+putchar:
+    mov ah, 0x0e  
+    mov bh, 0     
+    mov bl, 7
+    int 0x10     
+    ret
 
 .halt:
     jmp .halt
 
-read_input:
-    xor al, al
-    call getchar
-    mov [name], al
-    jmp main
 
-msg_hello: db "Enter 1 if you are gay or enter 2 if you are not gay", ENDL, 0
-msg_one: db "Nigga you gayyy", ENDL, 0
-msg_not_one: db "You aight", ENDL, 0
-name: resb 1
+
+msg_hello: db "Enter 2 numbers", ENDL, 0
+num1: resb 1
+num2: resb 1
+result: resb 1
+num_output: resb 2
 
 times 510-($-$$) db 0
 dw 0AA55h
