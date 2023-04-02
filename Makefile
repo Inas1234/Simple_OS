21bba1e107f823ef5e30c03f83413a09865480e1
Inas Malkic <inas.malkic@bicomsystems.com>
2023-04-02 16:02:02 +0200

    Checking for input

--- Makefile
+++ Makefile
@@ -5,8 +5,11 @@ BUILD_DIR=build
 
 $(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
 	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-	turncate -s 1440k $(BUILD_DIR)/main_floppy.img
+	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+	dd if=$(BUILD_DIR)/main.bin of=$@ bs=512 count=2880
 
 
 $(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
 	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin 
+
+
--- src/main.asm
+++ src/main.asm
@@ -12,6 +12,8 @@ puts:
     push si
     push ax
 
+
+
 .loop:
     lodsb
     or al, al
@@ -24,6 +26,16 @@ puts:
 .done:
     pop ax
     pop si
+    ret 
+
+getchar:
+    xor ah, ah
+    int 0x16
+    ret
+
+prompt:
+    mov si, msg_hello
+    call puts
     ret
 
 main:
@@ -32,16 +44,41 @@ main:
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
-    mov si, msg_hello
-    call puts
+    call prompt
+    call getchar
+    mov [name], al
+    cmp al, 0x31
+    je .is_one
+    jmp .not_one
     hlt
+
+
+.is_one:
+    mov si, msg_one
+    call puts
+    jmp read_input
+
+
+.not_one:
+    cmp al, 0x0D
+    je .halt
+    mov si, msg_not_one
+    call puts
+    jmp read_input
 
 .halt:
     jmp .halt
 
+read_input:
+    xor al, al
+    call getchar
+    mov [name], al
+    jmp main
 
-msg_hello: db "Hello, World!", ENDL, 0
-
+msg_hello: db "Enter 1 if you are gay or enter 2 if you are not gay", ENDL, 0
+msg_one: db "Nigga you gayyy", ENDL, 0
+msg_not_one: db "You aight", ENDL, 0
+name: resb 1
 
 times 510-($-$$) db 0
 dw 0AA55h
